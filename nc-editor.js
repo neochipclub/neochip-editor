732e377b384bb27eb13cfcf50f27cf3aa1b1972f
Hayeddine <hay.eddine8@gmail.com>
2023-12-10 11:36:08 +0100

    Updated: auto codemirror scripts import - V1.1.0

--- nc-editor.js
+++ nc-editor.js
@@ -1,45 +1,52 @@
-const root = document.getElementById("nc-code");
-const sourceCode = `// Get ready!\nvoid setup() {\n\tJoinCommunity();\n}\n\n// Let's do it!\nvoid loop() {\n\tDiscover();\n\tCollaborate();\n\tInovate();\n}`;
-root.innerHTML = sourceCode;
-const options = {
-    mode: "javascript",
-    theme: "gruvbox-dark",
-    lineNumbers: true,
-    autoCloseBrackets: true,
-    matchBrackets: true,
-}
-const editor = CodeMirror.fromTextArea(root, options);
+import "https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/codemirror.min.js";
+import "https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/mode/javascript/javascript.min.js";
+import "https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/addon/edit/closebrackets.min.js";
+import "https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/addon/edit/matchbrackets.min.js";
+
+(function () {
+    const root = document.getElementById("nc-code");
+    const sourceCode = `// Get ready!\nvoid setup() {\n\tJoinCommunity();\n}\n\n// Let's do it!\nvoid loop() {\n\tDiscover();\n\tCollaborate();\n\tInovate();\n}`;
+    root.innerHTML = sourceCode;
+    const options = {
+        mode: "javascript",
+        theme: "gruvbox-dark",
+        lineNumbers: true,
+        autoCloseBrackets: true,
+        matchBrackets: true,
+    }
+    const editor = CodeMirror.fromTextArea(root, options);
 
 
-const compileButton = document.querySelector("button.nc-compile");
-const uploadButton = document.querySelector("button.nc-upload");
-const messageBox = document.querySelector(".nc-state-message");
+    const compileButton = document.querySelector("button.nc-compile");
+    const uploadButton = document.querySelector("button.nc-upload");
+    const messageBox = document.querySelector(".nc-state-message");
 
-compileButton.addEventListener("click", () => showStateMessage("Compiling..."));
-uploadButton.addEventListener("click", () => showStateMessage("Uploading..."));
+    compileButton.addEventListener("click", () => showStateMessage("Compiling..."));
+    uploadButton.addEventListener("click", () => showStateMessage("Uploading..."));
 
 
-function showStateMessage(state = "Compiling...") {
-    disableButton(compileButton);
-    disableButton(uploadButton);
-    messageBox.classList.add("show");
-    messageBox.querySelector(".nc-state-label").innerHTML = state;
-    setTimeout(() => {
-        messageBox.querySelector(".nc-state-label").innerHTML = "Done";
-    }, 2000);
-    setTimeout(() => {
-        messageBox.classList.remove("show");
-        enableButton(compileButton)
-        enableButton(uploadButton)
-    }, 3000);
-}
+    function showStateMessage(state = "Compiling...") {
+        disableButton(compileButton);
+        disableButton(uploadButton);
+        messageBox.classList.add("show");
+        messageBox.querySelector(".nc-state-label").innerHTML = state;
+        setTimeout(() => {
+            messageBox.querySelector(".nc-state-label").innerHTML = "Done";
+        }, 2000);
+        setTimeout(() => {
+            messageBox.classList.remove("show");
+            enableButton(compileButton)
+            enableButton(uploadButton)
+        }, 3000);
+    }
 
-function disableButton(btn) {
-    btn.setAttribute("disabled", true);
-    btn.classList.add("disabled");
-}
+    function disableButton(btn) {
+        btn.setAttribute("disabled", true);
+        btn.classList.add("disabled");
+    }
 
-function enableButton(btn) {
-    btn.removeAttribute("disabled");
-    btn.classList.remove("disabled");
-}
+    function enableButton(btn) {
+        btn.removeAttribute("disabled");
+        btn.classList.remove("disabled");
+    }
+})()
